ae152052470c47750ff8de6de7ad2247a0c35561
Lara Sanders <larasanders@gmail.com>
2017-12-13 17:29:49 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Lara"
+  end
+  get '/hometown' do
+    "My hometown is Westport"
+  end
+  get '/favorite-song' do
+    "My favorite song is I cant decide"
+  end
+
 end
